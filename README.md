87eadfdbc8ec9d2f63ce2ebc2d54a2342eb5cf89
Mikhail Remizov <00keng@gmail.com>
2019-11-04 22:58:22 +0300

    Add win and vim script folders
    - Added windows init script
    - Added vimrc file
    - Updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dotfiles
 My dotfiles and work environment settings
+
+## /win
+
+This folder contains windows platform-specific scripts
+
+## /vimrc
+
+This folder contains my vim setup
---
+++ win/init.ps1
@@ -0,0 +1,9 @@
+# This script installs my typical software for a fresh windows machine
+# using the Chocolatey package manager. Read more about it at 
+# https://chocolatey.org/
+
+choco upgrade -y --no-progress googlechrome `
+      vim 7zip.install vlc git.install slack `
+      totalcommander docker-desktop dropbox `
+      telegram openvpn ccleaner geforce-experience `
+      obs-studio steam 
