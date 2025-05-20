44a32a23920345e3b39d47c2115f477740b5a6a5
mikhail remizov <00keng@gmail.com>
2025-05-20 23:57:17 +0300

    cleanup windows init script

--- win/init.ps1
+++ win/init.ps1
@@ -2,7 +2,7 @@
 # using the Chocolatey package manager. Read more about it at 
 # https://chocolatey.org/
 
-choco upgrade -y --no-progress firefox neovim 7zip.install vlc git.install `
-                               slack totalcommander docker-desktop dropbox `
-                               telegram openvpn ccleaner geforce-experience `
+choco upgrade -y --no-progress firefox neovim vlc git.install `
+                               totalcommander docker-desktop dropbox `
+                               telegram ccleaner geforce-experience `
                                obs-studio steam cheatengine treesizefree
